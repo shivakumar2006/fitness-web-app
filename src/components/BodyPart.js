273887b9aa88e16c19e5c809c011ea737a28b69b
shivakumar2006 <official.shivakumar06@gmail.com>
2025-01-22 19:45:20 +0530

    commit

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,10 +1,23 @@
 import React from 'react'; 
+import { Stack, Typography } from '@mui/material'
 
-const BodyPart = () => {
+import Icon from '../assets/icons/gym.png';
+
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
     return (
-        <div>
-            Bodypart 
-        </div>
+        <Stack
+            type='button'
+            alignItems='center'
+            justifyContent='center'
+            className='bodyPart-card'
+            sx={{
+                bodyPart === item? {
+                    
+                }
+            }}
+        >
+            <img src={Icon} alt='dumbbell' style={{ width: '40px', height: '40px' }} />
+        </Stack>
     )
 }
 
