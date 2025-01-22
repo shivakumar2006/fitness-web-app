8773e29fd53469c90f21e993f742d97c3f44b472
shivakumar2006 <official.shivakumar06@gmail.com>
2025-01-22 09:31:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material'; 
+
+import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
 
 const App = () => {
     return (
         <div>
-            App
+            <Box width="400px">
+                <Navbar />
+                <Route path='/' element={<Home />} />
+                <Route path="/exercise/:id" element={<ExerciseDetail />} />
+            </Box>
         </div>
     )
 }
