a2e564eb28e7079ae25c555daae22c10515c25a1
shivakumar2006 <official.shivakumar06@gmail.com>
2025-01-22 19:14:04 +0530

    add react version 18

--- src/components/HorizontalScrollBar.js
+++ src/components/HorizontalScrollBar.js
@@ -3,16 +3,21 @@ import { Box } from '@mui/material';
 
 import BodyPart from './BodyPart';
 
+
 const HorizontalScrollBar = ({ data, bodyPart, setBodyPart }) => {
+
+    
+
     return (
         <div>
             {data.map((item) => (
                 <Box 
                     key={item.id || item}
-                    itemId={item.id || item}
+                    itemID={item.id || item}
                     title={item.id || item}
                     m="0 40px"
                 >
+                    
                    <BodyPart item={item} bodyPart={bodyPart} setBodypart={setBodyPart} />
                 </Box>
             ))}
